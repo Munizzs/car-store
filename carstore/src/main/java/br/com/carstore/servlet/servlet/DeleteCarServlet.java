053f5d8df4696bdcb955982a93308591659ee70d
Matheus Muniz <matheusmuniz0810@gmail.com>
2024-10-13 18:25:22 -0300

    Implementação da requisição DELETE

---
+++ carstore/src/main/java/br/com/carstore/servlet/servlet/DeleteCarServlet.java
@@ -0,0 +1,25 @@
+package br.com.carstore.servlet.servlet;
+
+import br.com.carstore.servlet.dao.CarDao;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/delete-car")
+public class DeleteCarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        String carId = req.getParameter("id");
+
+        new CarDao().deleteCarById(carId);//Instanciou um objeto direto sem usar a variavel?
+
+        resp.sendRedirect("/find-all-cars");
+
+    }
+
+}
